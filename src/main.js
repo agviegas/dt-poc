2600bbe30992204083c7fdad7f6ddb026cee5782
Antonio González Viegas <antoniogviegas@hotmail.com>
2022-07-26 20:54:30 +0200

    Zip model

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import { ThreeScene } from './scene';
+import {unzip} from 'unzipit';
 import { FragmentLoader } from "bim-fragment/fragment-loader";
 
-const threeScene = new ThreeScene();
-const loader = new FragmentLoader();
+init();
 
-for(let i = 0; i < 5; i++) {
-    const geometryURL = `./model/${i}.glb`;
-    const dataURL = `./model/${i}.json`;
-    const fragment = await loader.load(geometryURL, dataURL);
-    threeScene.scene.add(fragment.mesh);
+async function init() {
+    const threeScene = new ThreeScene();
+    const loader = new FragmentLoader();
+
+    const {entries} = await unzip('model/model.zip');
+
+    const fileNames = Object.keys(entries);
+    for(let i = 0; i <= fileNames.length - 5; i+= 2) {
+        const geometryName = fileNames[i];
+        const geometry = await entries[geometryName].blob();
+        const geometryURL = URL.createObjectURL(geometry);
+
+        const dataName = fileNames[i + 1];
+        const data = await entries[dataName].blob();
+        const dataURL = URL.createObjectURL(data);
+
+        const fragment = await loader.load(geometryURL, dataURL);
+        threeScene.scene.add(fragment.mesh);
+    }
 }
